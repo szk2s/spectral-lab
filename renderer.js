be6663983f926d90e0ae01cf7cb83b1fda28c12d
szk2s <suzukisjss@gmail.com>
2018-12-21 23:07:55 +0900

    initializes with create-react-app and eslint

--- renderer.js
+++
@@ -1,8 +0,0 @@
-const s2m = require('./lib/spectrum2mpe');
-const environmentConfig = require('./config');
-
-const {filepath, partials} = require('./lib/buttons');
-require('./lib/viewer');
-console.log(filepath);
-
-
