46bc0efed9859f15bda0471a00191546a548ae86
Yuichi Yogo <yuichkun@gmail.com>
2018-12-23 02:49:22 +0900

    rewrite babel env

--- public/electron.js
+++ public/electron.js
@@ -2,14 +2,13 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
-const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
 	const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
 	mainWindow = new BrowserWindow({width, height});
-	mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+	mainWindow.loadFile(path.join(__dirname, 'index.html'));
 	// Open the DevTools.
 	mainWindow.webContents.openDevTools();
 	mainWindow.on('closed', () => mainWindow = null);
