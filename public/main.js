f96e36dd32393e3bbacd805f0dec4f82a29d765a
szk2s <suzukisjss@gmail.com>
2018-12-31 17:13:18 +0900

    Format code and add type checking

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const electron = require('electron');
 const app = electron.app;
 const globalShortcut = electron.globalShortcut;
@@ -8,27 +9,27 @@ global.ProgressBar = require('electron-progressbar');
 let mainWindow;
 
 function createWindow() {
-    const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
-    mainWindow = new BrowserWindow({ width, height });
-    mainWindow.loadFile(path.join(__dirname, 'index.html'));
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-    globalShortcut.register('CommandOrControl+R', () => {
-        mainWindow.reload();
-    });
-    mainWindow.on('closed', () => (mainWindow = null));
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+  mainWindow = new BrowserWindow({ width, height });
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+  globalShortcut.register('CommandOrControl+R', () => {
+    mainWindow.reload();
+  });
+  mainWindow.on('closed', () => (mainWindow = null));
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
